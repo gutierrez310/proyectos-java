7b1ce244ae6c246ee709c21bcc6a7cbeec9714a6
gutierrez310 <60931591+gutierrez310@users.noreply.github.com>
2020-06-05 16:32:09 -0500

    Add files via upload

---
+++ holamundo.java
@@ -0,0 +1,18 @@
+public class main {
+	//Atributos globales
+	//boolean isOn;
+	
+	//Metodos globales
+	//public boolean getIsOn() {
+	//	boolean isOn;
+	//	return this.isOn = isOn;
+	//}
+	
+	//Run|Debung
+	public static void main(String args[]) {
+
+	System.out.println("Hola Mundo");
+	
+	}
+
+}
